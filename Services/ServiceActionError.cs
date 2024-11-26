112afc19e68f1631a813fca41c9390c45468f760
yfranz <yfranz@gmail.com>
2024-11-25 18:24:49 -0800

    Initial push

---
+++ Services/ServiceActionError.cs
@@ -0,0 +1,7 @@
+﻿namespace DevInstance.BlazorToolkit.Services;
+
+public class ServiceActionError
+{
+    //TODO: This class will be extended with additional attributes
+    public string Message { get; set; }
+}
